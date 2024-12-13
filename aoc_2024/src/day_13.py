9eb5827a16d3945a649bc5f07a9115647ad9192b
BPR02 <dev@bpr02.com>
2024-12-12 23:00:50 -0800

    reformat days 12 and 13

--- aoc_2024/src/day_13.py
+++ aoc_2024/src/day_13.py
@@ -4,10 +4,11 @@ from pathlib import Path
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 
-from aoc_2024.lib.util import run
 import sympy as sp
+from sympy import Eq, var
 from sympy.solvers import solve
-from sympy import var, Eq
+
+from aoc_2024.lib.util import run
 
 YEAR = 2024
 DAY = 13
@@ -20,18 +21,19 @@ def solve_a(input: str) -> int | str | None:
         a = lines[i].removeprefix("Button A: X+").split(",")
         ay = int(a[1].removeprefix(" Y+"))
         ax = int(a[0])
-        b = lines[i+1].removeprefix("Button B: X+").split(",")
+        b = lines[i + 1].removeprefix("Button B: X+").split(",")
         by = int(b[1].removeprefix(" Y+"))
         bx = int(b[0])
-        p = lines[i+2].removeprefix("Prize: X=").split(",")
+        p = lines[i + 2].removeprefix("Prize: X=").split(",")
         py = int(p[1].removeprefix(" Y="))
         px = int(p[0])
-        
+
         total += count_tokens(ax, ay, bx, by, px, py)
     return total
 
+
 def count_tokens(ax: int, ay: int, bx: int, by: int, px: int, py: int):
-    possible: list[int] = []
+    possible: list[tuple[int, int]] = []
     for a in range(100):
         check_ax = ax * a
         check_ay = ay * a
@@ -53,10 +55,11 @@ def count_tokens(ax: int, ay: int, bx: int, by: int, px: int, py: int):
     if len(possible) == 0:
         return 0
     min_token = 10000
-    for (a, b) in possible:
+    for a, b in possible:
         token = a * 3 + b
         min_token = min(min_token, token)
     return min_token
+
 
 def solve_b(input: str) -> int | str | None:
     lines = input.splitlines()
@@ -65,10 +68,10 @@ def solve_b(input: str) -> int | str | None:
         a = lines[i].removeprefix("Button A: X+").split(",")
         ay = int(a[1].removeprefix(" Y+"))
         ax = int(a[0])
-        b = lines[i+1].removeprefix("Button B: X+").split(",")
+        b = lines[i + 1].removeprefix("Button B: X+").split(",")
         by = int(b[1].removeprefix(" Y+"))
         bx = int(b[0])
-        p = lines[i+2].removeprefix("Prize: X=").split(",")
+        p = lines[i + 2].removeprefix("Prize: X=").split(",")
         py = int(p[1].removeprefix(" Y="))
         px = int(p[0])
         px += 10000000000000
@@ -77,29 +80,33 @@ def solve_b(input: str) -> int | str | None:
         total += calc(ax, ay, bx, by, px, py)
     return total
 
-def calc(ax: int, ay: int, bx: int, by: int, px: int, py: int):
-    var('va vb')
-    x = Eq(ax*va+bx*vb,px) 
-    y = Eq(ay*va+by*vb,py)
-    possible = solve([x,y],dict=True)
+
+def calc(ax: int, ay: int, bx: int, by: int, px: int, py: int) -> int:
+    var("va vb")
+    x = Eq(ax * va + bx * vb, px)  # type: ignore
+    y = Eq(ay * va + by * vb, py)  # type: ignore
+    possible = solve([x, y], dict=True)
     if len(possible) == 0:
         return 0
     min_token = None
     for d in possible:
-        a = d[va]
-        b = d[vb]
-        if not isinstance(a,sp.core.numbers.Integer) or not isinstance(b,sp.core.numbers.Integer):
+        a = d[va]  # type: ignore
+        b = d[vb]  # type: ignore
+        if not isinstance(a, sp.core.numbers.Integer) or not isinstance(
+            b, sp.core.numbers.Integer
+        ):
             continue
         if a < 0 or b < 0:
             continue
-        token = a * 3 + b
-        if min_token is None: 
+        token: int = int(a * 3 + b)
+        if min_token is None:
             min_token = token
         else:
             min_token = min(min_token, token)
     if min_token is None:
         return 0
-    return min_token
+    return int(min_token)
+
 
 if __name__ == "__main__":
     import argparse
